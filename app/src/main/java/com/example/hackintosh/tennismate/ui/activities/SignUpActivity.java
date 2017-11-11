1900dbe3f07e8c2f03fa5caed6ec8d1470cc768a
Luca Schidu <schiduluca@gmail.com>
2017-11-11 21:29:45 +0200

    Added user dto, signun partial implementation

--- app/src/main/java/com/example/hackintosh/tennismate/ui/activities/SignUpActivity.java
+++ app/src/main/java/com/example/hackintosh/tennismate/ui/activities/SignUpActivity.java
@@ -1,12 +1,17 @@
 package com.example.hackintosh.tennismate.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.example.hackintosh.tennismate.R;
+import com.example.hackintosh.tennismate.dto.DataKeys;
+import com.example.hackintosh.tennismate.dto.User;
 import com.example.hackintosh.tennismate.ui.navigation.Navigator;
 import com.example.hackintosh.tennismate.ui.presenters.SignUpPresenter;
 import com.example.hackintosh.tennismate.ui.view.SingnUpView;
+
+import java.io.Serializable;
 
 /**
  * Created by maxim on 11/11/17.
@@ -18,6 +23,10 @@ public class SignUpActivity extends BaseActivity<SingnUpView, SignUpPresenter> i
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         setPresenter();
+
+        Intent intent = this.getIntent();
+        Bundle bundle = intent.getExtras();
+        User serializable = (User) bundle.getSerializable(DataKeys.USER);
     }
 
     public void setPresenter() {
