fd5a7c9ae972e011d24c59979bf622ac500582e9
Nicu Maxian <maxiannicu@gmail.com>
2017-11-11 22:27:07 +0200

    Added firebase database

--- app/src/main/java/com/example/hackintosh/tennismate/ui/activities/SignUpActivity.java
+++ app/src/main/java/com/example/hackintosh/tennismate/ui/activities/SignUpActivity.java
@@ -3,10 +3,13 @@ package com.example.hackintosh.tennismate.ui.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.example.hackintosh.tennismate.R;
 import com.example.hackintosh.tennismate.dto.DataKeys;
+import com.example.hackintosh.tennismate.dto.LevelEnum;
 import com.example.hackintosh.tennismate.dto.User;
+import com.example.hackintosh.tennismate.service.UserService;
 import com.example.hackintosh.tennismate.ui.navigation.Navigator;
 import com.example.hackintosh.tennismate.ui.presenters.SignUpPresenter;
 import com.example.hackintosh.tennismate.ui.view.SingnUpView;
@@ -27,6 +30,9 @@ public class SignUpActivity extends BaseActivity<SingnUpView, SignUpPresenter> i
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
         User serializable = (User) bundle.getSerializable(DataKeys.USER);
+
+        UserService userService = new UserService();
+        userService.signUp(new User("nicu.maxian@gmail.com",null,"Nicu Maxian", LevelEnum.ADVANCED,"http://google.com"),() -> Log.d("test","Finished"),(err) -> Log.e("test",err));
     }
 
     public void setPresenter() {
