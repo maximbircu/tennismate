26458dd990b708820fe8b7998f9c8e02faec823e
Nicu Maxian <maxiannicu@gmail.com>
2017-11-12 05:00:58 +0200

    Added navigation

--- app/src/main/java/com/example/hackintosh/tennismate/ui/activities/PlanGameActivity.java
+++ app/src/main/java/com/example/hackintosh/tennismate/ui/activities/PlanGameActivity.java
@@ -2,11 +2,17 @@ package com.example.hackintosh.tennismate.ui.activities;
 
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.hackintosh.tennismate.InstanceData;
 import com.example.hackintosh.tennismate.R;
 import com.example.hackintosh.tennismate.ui.navigation.Navigator;
 import com.example.hackintosh.tennismate.ui.presenters.RandomPartnerFinderPresenter;
 import com.example.hackintosh.tennismate.ui.view.RandomPartnerFinderView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class PlanGameActivity extends BaseAuthenticatedActivity<RandomPartnerFinderView, RandomPartnerFinderPresenter> implements RandomPartnerFinderView {
@@ -21,6 +27,19 @@ public class PlanGameActivity extends BaseAuthenticatedActivity<RandomPartnerFin
     protected void onResume() {
         super.onResume();
         super.setContentLayout(R.layout.activity_plan_game);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.single_mode)
+    public void onSingleModeClick(){
+        InstanceData.players = 1;
+        presenter.navigator.openCourtListActivity();
+    }
+
+    @OnClick(R.id.double_mode)
+    public void onDoubleModeClick(){
+        InstanceData.players = 3;
+        presenter.navigator.openCourtListActivity();
     }
 
     public void setPresenter() {
